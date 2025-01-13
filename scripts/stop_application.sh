b5dc1b2fc54bbda85b9b1119e544d62edf0e7397
Mike Cherneski <mgcherneski@gmail.com>
2025-01-13 14:40:51 -0500

    updated stop application script

--- scripts/stop_application.sh
+++ scripts/stop_application.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-# Stop the service if it's running
-if systemctl is-active --quiet event-listener; then
-    systemctl stop event-listener
+# Check if the service exists
+if systemctl list-unit-files | grep -q event-listener; then
+    # Stop the service if it's running
+    if systemctl is-active --quiet event-listener; then
+        systemctl stop event-listener
+    fi
+    # Disable the service
+    systemctl disable event-listener
 fi
 
-# Disable the service
-systemctl disable event-listener 
+# Always exit successfully
+exit 0 
