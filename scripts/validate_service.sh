f14a433c1bbb0a54e0b263cb396becb00d40ea2e
Mike Cherneski <mgcherneski@gmail.com>
2025-02-06 14:26:16 -0500

    updated validate service script.

--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Check if service is running
-if ! systemctl is-active --quiet event-listener; then
-    echo "event-listener service is not running"
+if ! systemctl is-active --quiet event-monitor; then
+    echo "event-monitor service is not running"
     exit 1
 fi
 
